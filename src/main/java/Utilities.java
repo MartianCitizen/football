035d147d6f401de0e849cdf1164ed3bcdbc0676c
John Chamberlain <MartianCitizen@users.noreply.github.com>
2016-10-06 07:50:33 -0400

    Create Utilities.java

---
+++ src/main/java/Utilities.java
@@ -0,0 +1,35 @@
+package com.martiancitizen.football;
+
+import java.util.function.Supplier;
+
+/**
+ * Created by johnchamberlain on 10/4/16.
+ */
+public class Utilities {
+
+    public static final Supplier<AssertionError> OVE = () -> new AssertionError("Object validator not called");
+
+    public static <T> T requiredArg(T arg) {
+        if (arg == null) {
+            throw new IllegalArgumentException("missing argument");
+        }
+        if (arg instanceof String && ((String) arg).isEmpty()) {
+            throw new IllegalArgumentException("empty string argument");
+        }
+        return arg;
+    }
+
+    public static <T> boolean isSupplied(T arg) {
+        if (arg == null) {
+            return false;
+        }
+        if (arg instanceof String && ((String) arg).isEmpty()) {
+            return false;
+        }
+        return true;
+    }
+
+    public static String convertEmptyToNull(String arg) {
+        return arg == null ? null : (arg.isEmpty() ? null : arg);
+    }
+}
